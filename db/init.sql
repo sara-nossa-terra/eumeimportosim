fba592bf4df39e38c45b936b5eac7db04b0a6ddc
Paulo Henrique <paulotux.mendes@gmail.com>
2020-09-04 21:15:54 -0300

    [UPDATE] Criando script pra alteracão do host para localhost

---
+++ db/init.sql
@@ -0,0 +1,4 @@
+
+UPDATE `eumeimportosim_wp`.`wp_options` SET `option_value` = 'http://localhost/' WHERE (`option_id` = '1');
+
+UPDATE `eumeimportosim_wp`.`wp_options` SET `option_value` = 'http://localhost/' WHERE (`option_id` = '2');
